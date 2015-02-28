e5c6a7e36a8bd1c929cf71fc50a0f7f3f4000d95
Isaac <yzaak.silva@gmail.com>
2015-02-28 00:50:43 -0300

    Revert "commit 4"
    This reverts commit b7aa363baa61aba08ab30a426ccd6d3a8b148b0e.

--- aula2/Aula2.java
+++ aula2/Aula2.java
@@ -1,7 +1,7 @@
 package aula2;
 public class Aula2{
 	public static void main(String[] args) {
-		Vertice v = new Vertice(3,8);
+		Vertice v = new Vertice(3,5);
 		System.out.println("v("+v.getX()+","+v.getY()+")");
 	}
 }
